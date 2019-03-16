8e2170c91dd160c0b6892a787bc2ed6fe5e087cf
Mariocoski <mariuszrajczakowski@gmail.com>
2019-03-16 00:44:01 +0000

    changed config.yml

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
 // tslint:disable-next-line:no-import-side-effect
 import './App.css';
-import logo from './logo.svg';
 
 const App = () => (
   <div className="App">
     <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
       <p>
         Edit <code>src/App.tsx</code> and save to reload.
       </p>
