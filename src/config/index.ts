bf472ad455cc9003222786b0df88d0f11639b094
kube-js <mariuszrajczakowski@pm.me>
2019-07-26 17:21:14 +0100

    feat: added loading env variables mechanism for docker container

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,11 +1,14 @@
-import getStringValue from "../utils/helpers/getStringValue";
+import getStringValue from '../utils/helpers/getStringValue';
 
 export interface Config {
   apiUrl: string;
 }
 
 const config: Config = {
-  apiUrl: getStringValue(process.env.REACT_APP_API_URL, 'http://localhost:9000/api/v1'),
+  apiUrl: getStringValue(
+    (window as any)._env_.REACT_APP_API_URL,
+    'http://localhost:9000/api/v1'
+  ),
 };
 
 export default config;
