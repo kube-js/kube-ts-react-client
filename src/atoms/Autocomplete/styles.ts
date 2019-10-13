de6e701f86d6794058df701a777143188eda3880
kube-js <mariuszrajczakowski@pm.me>
2019-10-13 21:18:22 +0100

    changed styling of autocomplete component

--- src/atoms/Autocomplete/styles.ts
+++ src/atoms/Autocomplete/styles.ts
@@ -2,18 +2,35 @@
 import { makeStyles, Theme } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme: Theme) => ({
+  closeIcon: {
+    cursor: 'pointer',
+    right: 0,
+  },
   container: {
+    display: 'flex',
     flexGrow: 1,
     position: 'relative',
+    width: '100%',
   },
   divider: {
     height: theme.spacing(2),
+  },
+  icon: {
+    alignItems: 'center',
+    display: 'flex',
+    height: '100%',
+    justifyContent: 'center',
+    position: 'absolute',
+    width: theme.spacing(6),
+    zIndex: 2,
   },
   inputInput: {
     background: '#fff',
     borderRadius: '5px',
     color: '#000',
     flexGrow: 1,
+    paddingLeft: '48px',
+    paddingRight: '48px',
     width: 'auto',
   },
   inputRoot: {
